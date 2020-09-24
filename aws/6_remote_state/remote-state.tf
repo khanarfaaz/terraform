a50ce88dd133d851be9b13898f90a33787b68bb0
khanarfaaz <khanarfaaz@gmail.com>
2020-09-24 17:19:31 +0530

    Update remote-state.tf

--- aws/6_remote_state/remote-state.tf
+++ aws/6_remote_state/remote-state.tf
@@ -3,8 +3,8 @@ terraform {
     bucket  = "cloud9-epl"
     key  = "terraform/state"
     region = "us-east-2"
-#   access_key = "AKIAZ3S6MYW2XDP6GOWH"
-#   secret_key = "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"
+#   access_key = ""
+#   secret_key = ""
   }
 }
 
