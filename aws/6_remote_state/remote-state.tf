de43f2514317f61eadc72701b491e3db0d195875
khanarfaaz <khanarfaaz@gmail.com>
2020-09-24 16:59:02 +0530

    Update remote-state.tf

--- aws/6_remote_state/remote-state.tf
+++ aws/6_remote_state/remote-state.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket  = "techlanders-statefile"
+    bucket  = "cloud9-epl"
     key  = "terraform/state"
     region = "us-east-2"
-#   access_key = "XXXXXXXXXXXXXXXXXXXXXX"
-#   secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#   access_key = "AKIAZ3S6MYW2XDP6GOWH"
+#   secret_key = "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"
   }
 }
 
