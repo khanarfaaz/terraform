c228960443118a07b0cb90c6a374b89e495db2d1
khanarfaaz <khanarfaaz@gmail.com>
2020-09-24 17:53:16 +0530

    Update ec2.tf

--- aws/6_remote_state/ec2.tf
+++ aws/6_remote_state/ec2.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "EPL" {
+resource "aws_instance" "EPL1" {
   ami = "ami-0603cbe34fd08cb81"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "EPL"
+    Name = "EPL1"
     Env = "Cloud"
   }
 }
 
 output "EPL-ip" {
-  value = "${aws_instance.EPL.public_ip}"
+  value = "${aws_instance.EPL1.public_ip}"
 }
 
